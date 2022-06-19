010b3c5cd3854f88e2bfefec524b4fa4dc852cce
manasjain0405 <manas.jain@groww.in>
2022-06-20 02:03:03 +0530

    Bind Question

--- assignment3/string_validity.js
+++ assignment3/string_validity.js
@@ -1,11 +1,29 @@
 function validateString(input, callback) {
   
-    setTimeout(function () {
-    // input is said to be valid if it is a lowercase string
-    if (typeof input === "string" && input === input.toLowerCase()) {
-      return callback(null, true)
-    }
-    return callback(new Error('Invalid string'), null)
-  }, 500)
+  setTimeout(function () {
+  // input is said to be valid if it is a lowercase string
+  if (typeof input === "string" && input === input.toLowerCase()) {
+    return callback(null, true)
+  }
+  return callback(new Error('Invalid string'), null)
+}, 500)
 }
 
+const res = {}
+function get_callback(x) {
+  
+  return function(a, b) {
+      if(b) {
+          res[x] = true;
+      }else {
+          res[x] = false;
+      }
+      console.log(res);
+  }
+}
+
+
+const input = ['first', 'Second', 'thiRd', 4, false, 'true'];
+input
+.map((x) => validateString.bind(null, x, get_callback(x)))
+.forEach( (x) => x())
