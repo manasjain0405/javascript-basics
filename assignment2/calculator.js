8ca266b1d6ca66934bfa3f280feb6c4ba409a6b3
manasjain0405 <manas.jain@groww.in>
2022-06-20 22:35:55 +0530

    Changes

--- assignment2/calculator.js
+++ assignment2/calculator.js
@@ -1,5 +1,4 @@
 class Calculator {
-<<<<<<< HEAD
   constructor() {
     this.a = 0;
     this.b = 0;
@@ -21,26 +20,3 @@ const calc = new Calculator();
 calc.read(5, 10);
 console.log(calc.sum());
 console.log(calc.mul());
-=======
-
-    constructor() {
-        this.a = 0;
-        this.b = 0;
-        this.read = function(a, b) {
-            this.a = a;
-            this.b = b;
-        };
-        this.sum = function() {
-            return this.a + this.b;
-        };
-        this.mul = function() {
-            return this.a * this.b;
-        }
-    };
-  }
-  
-const calc = new Calculator();
-calc.read(5,10);
-console.log(calc.sum())
-console.log(calc.mul())
->>>>>>> 2d48d5895da26e79ea0cc54a14fd6ff8e84d46a8
--- assignment2/check_propercase.js
+++ assignment2/check_propercase.js
@@ -1,5 +1,4 @@
 function convertProperCase(str) {
-<<<<<<< HEAD
   if (str.length <= 0) {
     return;
   }
@@ -12,16 +11,3 @@ function convertProperCase(str) {
 // Test
 console.log(convertProperCase('manas'));
 console.log(convertProperCase('Jain'));
-=======
-    if(str.length <= 0){
-        return;
-    }
-    if(str[0] == str[0].toLowerCase()) {
-        return str[0].toUpperCase().concat(str.substring(1))
-    }
-    return str;
-}
-
-console.log(convertProperCase("manas"))
-console.log(convertProperCase("Jain"))
->>>>>>> 2d48d5895da26e79ea0cc54a14fd6ff8e84d46a8
